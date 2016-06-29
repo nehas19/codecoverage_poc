848b1f82d751b20658b0a868e547db929a154b61
arpitga <arpitga@ARPITGA-W8>
2016-06-29 15:05:14 +0530

    opencover and reportgenerator added at 15:05

--- UnitTestProject.Test/TestClass.cs
+++ UnitTestProject.Test/TestClass.cs
@@ -17,8 +17,6 @@ namespace UnitTestProject.Test
             MathsHelper helper = new MathsHelper();
             int result = helper.Add(20, 10);
             Assert.AreEqual(30, result);
-
-
         }
 
         [TestCase]
